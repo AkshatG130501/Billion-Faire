50c4367a3269dac7a6f2927554f4d2a45b94bd40
Akshat Gupta <guptaakshat370@gmai.com>
2024-03-26 12:35:58 +0530

    fix: login request url change

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-		proxy: {
-			"/api": {
-				target: "https://billion-faire.onrender.com",
-			},
-		},
-	},
+//   server: {
+//     port: 3000,
+// 		proxy: {
+// 			"/api": {
+// 				target: "https://billion-faire.onrender.com",
+// 			},
+// 		},
+// 	},
 })
